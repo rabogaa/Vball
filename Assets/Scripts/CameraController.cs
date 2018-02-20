fb7c46fedd54bea711a8baf28ea7da76f5f7382f
Roberto Abogaa <4242309+rabogaa@users.noreply.github.com>
2018-02-20 09:16:24 +0800

    Added camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private bool _gameover;
+
+    [SerializeField]
+    private float _learpRate;
+    
+    // Use this for initialization
+    void Start()
+    {
+        if (_learpRate < 1)
+        {
+            _learpRate = 2;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
